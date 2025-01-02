30c36e1aba333fda4f62b26cf61075bdc326fb6b
matosluizdev <luizmatosdev@outlook.com>
2025-01-02 17:10:30 -0300

    feat: adding src and border as props

--- React/01-fundamentosReac/src/components/Post.jsx
+++ React/01-fundamentosReac/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 export function Post() {
@@ -5,7 +6,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/matosluizdev.png" />
+                    <Avatar src="https://github.com/matosluizdev.png" />
                     <div className={styles.authorInfo}>
                         <strong>Luiz Matos</strong>
                         <span>Web Developer</span>
--- React/01-fundamentosReac/src/components/Sidebar.jsx
+++ React/01-fundamentosReac/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css'
 import { Pencil } from "phosphor-react";
 
@@ -10,7 +11,7 @@ return (
         src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
         />
         <div className={styles.profile}>
-            <img className={styles.avatar} src="https://github.com/matosluizdev.png" />
+            <Avatar hasBorder src="https://github.com/matosluizdev.png" />        
             <strong>Luiz Matos</strong>
             <span>Web Developer</span>
             <footer>      
