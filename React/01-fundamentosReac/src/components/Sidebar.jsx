7b536ab4b554b938b70f399db51958ae79f29204
matosluizdev <luizmatosdev@outlook.com>
2025-01-02 15:17:38 -0300

    feat: finishing sidebar

--- React/01-fundamentosReac/src/components/Sidebar.jsx
+++ React/01-fundamentosReac/src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styles from './Sidebar.module.css'
+import { Pencil } from "phosphor-react";
+
 
 export function Sidebar() {
 return (
@@ -8,10 +10,13 @@ return (
         src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
         />
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/matosluizdev.png" />
             <strong>Luiz Matos</strong>
             <span>Web Developer</span>
-            <footer>           
-                 <a href="#">Editar seu perfil</a>
+            <footer>      
+                <a href="#">
+                    <Pencil size={20} /> Editar seu perfil
+                </a>
             </footer>
         </div>
     </aside>
