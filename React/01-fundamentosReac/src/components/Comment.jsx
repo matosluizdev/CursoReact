206d71660a2133c64a692b45b4d4fc1973c5ae6c
matosluizdev <luizmatosdev@outlook.com>
2025-01-02 16:52:10 -0300

    feat: finished in the comment component

---
+++ React/01-fundamentosReac/src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/matosluizdev.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Luiz Matos</strong>
+                            <time title='31 de Dezembro de 2024 as 08:15h' dateTime='2024-12-31 08:13:00'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar Comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Very Good!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
